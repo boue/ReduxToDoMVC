134d5ef45881d000d11cd9ae7415eea8f7dcfc08
Germain Boue <boue.germain@gmail.com>
2016-03-30 21:27:50 -0700

    First passing test in order to filter by active

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 export default class TodoList extends Component {
+  //filters items according to their status
+  getItems(){
+    if(this.props.todos){
+      return this.props.todos.filter(
+        (item) => item.get('status') === this.props.filter
+      );
+    }
+    return [];
+  }
   render(){
     return(
       <section className="main">
         <ul className="todo-list">
-          {this.props.todos.map(item =>
+          {this.getItems().map(item =>
             <TodoItem key={item.get('text')}
                       text={item.get('text')} />
           )}
