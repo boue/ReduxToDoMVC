9a0482d198a1bf9fa06b7d25e373cdbe7cb4908e
Germain Boue <boue.germain@gmail.com>
2016-03-30 21:02:53 -0700

    Build TodoApp component, TodoList where todos are passed in as props and mapped for the TodoItem which has yet to be created

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends Component {
+  render(){
+    return(
+      <section className="main">
+        <ul className="todo-list">
+          {this.props.todos.map(item =>
+            <TodoItem key={item.get('text')}
+                      text={item.get('text')} />
+          )}
+        </ul>
+      <section />
+    );
+  }
+}
