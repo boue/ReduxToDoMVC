16daef843abbc2e91f9d51b22ff23d39eca39398
Germain Boue <boue.germain@gmail.com>
2016-03-30 20:54:42 -0700

    Add css package, install loaders for webpack, and load in css styling inside index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080', // Setting the URL for the hot reload
     'webpack/hot/only-dev-server', // Reload only the dev server
-    './src/index.js'
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'react-hot!babel' // Include the react-hot loader
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
     }]
   },
   resolve: {
