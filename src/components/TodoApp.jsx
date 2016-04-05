1244cef9359df1e7fe649e6df5e38a6735d28d7e
Germain Boue <boue.germain@gmail.com>
2016-04-04 19:23:08 -0700

    TodoHeader tests with implementation of component

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TodoList from './Todolist';
+import TodoHeader from './TodoHeader';
 
 export default class TodoApp extends Component {
   render(){
     return (
       <div>
         <section className="todoapp">
+          <TodoHeader />
           <TodoList todos={this.props.todos} filter={this.props.filter} />
         </section>
       </div>
