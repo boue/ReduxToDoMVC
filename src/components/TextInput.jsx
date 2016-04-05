d2bcec2e6b8255fde1e00b2e617da0060e26d92c
Germain Boue <boue.germain@gmail.com>
2016-04-04 18:25:43 -0700

    implement text_input properly

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,7 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 
-export default class TextInput extends Component {
-  render() {
-    return <input type="text" className="edit" autoFocus={true} />
+export default React.createClass({
+  getInitialState: function() {
+    return {value: this.props.text};
+  },
+  _handleKeyDown: function(e) {
+    switch (e.key) {
+      case 'Enter':
+        return this.props.doneEditing(this.props.itemId, this.state.value);
+      case 'Escape':
+        return this.cancelEditing(this.props.itemId);
+    }
+  },
+  _handleOnBlur: function(e) {
+    return this.cancelEditing(this.props.itemId);
+  },
+  _handleOnChange: function(e) {
+    this.setState({value: e.target.value});
+  },
+  cancelEditing: function() {
+    this.setState({'value': this.props.text});
+    return this.props.cancelEditing(this.props.itemId);
+  },
+  render: function () {
+    return <input className="edit"
+                  autoFocus={true}
+                  value={this.state.value}
+                  onChange={this._handleOnChange}
+                  type="text"
+                  ref="itemInput"
+                  onKeyDown={this._handleKeyDown}
+                  onBlur={this._handleOnBlur}
+                  />
   }
-};
+});
